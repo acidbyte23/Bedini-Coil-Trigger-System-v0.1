838f2d736c296745281a5fe05ddbdf10546262e3
JeroenvanDijk23 <88032009+JeroenvanDijk23@users.noreply.github.com>
2022-09-11 16:16:09 +0200

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,81 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Voltage_In_Pin GPIO_PIN_0
+#define Voltage_In_GPIO_Port GPIOC
+#define Current_In_Pin GPIO_PIN_1
+#define Current_In_GPIO_Port GPIOC
+#define Delay_In_Pin GPIO_PIN_2
+#define Delay_In_GPIO_Port GPIOC
+#define Width_In_Pin GPIO_PIN_3
+#define Width_In_GPIO_Port GPIOC
+#define Hall_Trigger_In_Pin GPIO_PIN_0
+#define Hall_Trigger_In_GPIO_Port GPIOB
+#define Hall_Trigger_In_EXTI_IRQn EXTI0_IRQn
+#define Inductor_Pulse_Pin GPIO_PIN_1
+#define Inductor_Pulse_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
